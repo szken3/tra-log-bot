1b14ef9fae54a12a8150cd23d94f9000dee13809
Ken Suzuki <dddlia3@gmail.com>
2022-02-25 21:57:20 +0900

    google spreadsheet api test

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 app = Flask(__name__)
 
 #環境変数取得
@@ -19,6 +22,29 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+# 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
+credential = {
+                "type": "service_account",
+                "project_id": os.environ['SHEET_PROJECT_ID'],
+                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+                "private_key": os.environ['SHEET_PRIVATE_KEY'],
+                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+                "client_id": os.environ['SHEET_CLIENT_ID'],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+             }
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+
+gc = gspread.authorize(credentials)
+
+wks = gc.open('Where is the money Lebowski?').sheet1
 
 @app.route("/")
 def hello_world():
@@ -43,6 +69,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    wks = gc.open('Where is the money Lebowski?').sheet1
     line_bot_api.reply_message(
         event.reply_token,
         # TextSendMessage(text=event.message.text))
