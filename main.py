8682832da4efebd072deb834a54280e77cae8e62
Ken Suzuki <dddlia3@gmail.com>
2022-02-25 22:05:39 +0900

    google api test

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    wks = gc.open('Where is the money Lebowski?').sheet1
+    wks = gc.open('トレーニング記録').sheet1
+    wks.update_cell(1, 1, "test")
     line_bot_api.reply_message(
         event.reply_token,
         # TextSendMessage(text=event.message.text))
