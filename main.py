0be1f4c42ccbda4943606cf03945ddc4cd814320
Ken Suzuki <dddlia3@gmail.com>
2022-02-26 16:02:21 +0900

    test cord

--- main.py
+++ main.py
@@ -29,23 +29,23 @@ scope = ['https://spreadsheets.google.com/feeds',
 print(os.environ['SHEET_PRIVATE_KEY'])
 
 # 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
-credential = {
-                "type": "service_account",
-                "project_id": os.environ['SHEET_PROJECT_ID'],
-                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
-                #"private_key": os.environ['SHEET_PRIVATE_KEY'],
-                "private_key": os.environ['SHEET_PRIVATE_KEY'].replace('\n', '\\n'),
-                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
-                "client_id": os.environ['SHEET_CLIENT_ID'],
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
-             }
-
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
-
-gc = gspread.authorize(credentials)
+# credential = {
+#                 "type": "service_account",
+#                 "project_id": os.environ['SHEET_PROJECT_ID'],
+#                 "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+#                 #"private_key": os.environ['SHEET_PRIVATE_KEY'],
+#                 "private_key": os.environ['SHEET_PRIVATE_KEY'].replace('\n', '\\n'),
+#                 "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+#                 "client_id": os.environ['SHEET_CLIENT_ID'],
+#                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#                 "token_uri": "https://oauth2.googleapis.com/token",
+#                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#                 "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+#              }
+#
+# credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+#
+# gc = gspread.authorize(credentials)
 
 @app.route("/")
 def hello_world():
@@ -70,8 +70,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    wks = gc.open('トレーニング記録').sheet1
-    wks.update_cell(1, 1, "test")
+    #wks = gc.open('トレーニング記録').sheet1
+    #wks.update_cell(1, 1, "test")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
