8a9e36be4fae45845cf1e7bceca5ff7ead0c49b6
Ken Suzuki <dddlia3@gmail.com>
2022-02-26 16:13:50 +0900

    test

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ scope = ['https://spreadsheets.google.com/feeds',
 
 print(os.environ['SHEET_PRIVATE_KEY'])
 
-辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
+# 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
 credential = {
                 "type": "service_account",
                 "project_id": os.environ['SHEET_PROJECT_ID'],
                 "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
                 "private_key": os.environ['SHEET_PRIVATE_KEY'],
-                # "private_key": os.environ['SHEET_PRIVATE_KEY'].replace('\n', '\\n'),
+                #"private_key": os.environ['SHEET_PRIVATE_KEY'].replace('\n', '\\n'),
                 "client_email": os.environ['SHEET_CLIENT_EMAIL'],
                 "client_id": os.environ['SHEET_CLIENT_ID'],
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
