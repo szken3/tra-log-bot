a2a69cb22f32970ca030830c6be98cf95adf3b03
Ken Suzuki <dddlia3@gmail.com>
2022-03-02 20:59:09 +0900

    test

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    wks = gc.open('test').sheet1
+    wks = gc.open('トレーニング記録').sheet1
     wks.update_cell(1, 1, "test")
     line_bot_api.reply_message(
         event.reply_token,
