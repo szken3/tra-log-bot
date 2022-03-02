7a3555aae1e60cc9d7f8d957798e62aae6efb1ab
Ken Suzuki <dddlia3@gmail.com>
2022-03-02 21:10:34 +0900

    tres

--- main.py
+++ main.py
@@ -35,8 +35,9 @@ credential = {
                 "type": "service_account",
                 "project_id": os.environ['SHEET_PROJECT_ID'],
                 "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
-                "private_key": os.environ['SHEET_PRIVATE_KEY'],
+                # "private_key": os.environ['SHEET_PRIVATE_KEY'],
                 # "private_key": os.environ['SHEET_PRIVATE_KEY'].replace('/n','/r'),
+                "private_key": os.environ.get('SHEET_PRIVATE_KEY').replace('\n', '\n'),
                 "client_email": os.environ['SHEET_CLIENT_EMAIL'],
                 "client_id": os.environ['SHEET_CLIENT_ID'],
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -44,6 +45,8 @@ credential = {
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
                 "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
              }
+
+
 
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
 
